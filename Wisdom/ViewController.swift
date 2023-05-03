7583fcb9f0204a3f6a3cf5d4ae3556ff00920c50
Arunprasath <arunnivi13@gmail.com>
2023-05-03 22:46:01 +0530

    Initial Commit

---
+++ Wisdom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wisdom
+//
+//  Created by Macbook  on 03/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
